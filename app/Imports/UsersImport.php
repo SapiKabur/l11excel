00907c06d4a0979eab41fc09f9918c2c7c368b83
SapiKabur <bayharisman89@gmail.com>
2024-05-10 01:43:09 +0700

    update

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -1,11 +1,14 @@
 <?php
-
+  
 namespace App\Imports;
-
+  
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\ToModel;
-
-class UsersImport implements ToModel
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
+use Hash;
+  
+class UsersImport implements ToModel, WithHeadingRow, WithValidation
 {
     /**
     * @param array $row
@@ -15,7 +18,23 @@ class UsersImport implements ToModel
     public function model(array $row)
     {
         return new User([
-            //
+            'name'     => $row['name'],
+            'email'    => $row['email'], 
+            'password' => Hash::make($row['password']),
         ]);
+    }
+  
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'password' => 'required|min:5',
+            'email' => 'required|email|unique:users'
+        ];
     }
 }
